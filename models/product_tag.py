67ccd3516967d338c1a996cf4d34a8ad030e688f
micheledic <dicroce.m@stesi.eu>
2024-05-28 14:45:49 +0200

    up

--- models/product_tag.py
+++ models/product_tag.py
@@ -5,3 +5,8 @@ from odoo import api, fields, models
 class PT(models.Model):
     _inherit = 'product.tag'
     code = fields.Char()
+    def _compute_display_name(self):
+        super()._compute_display_name()
+        for pt in self:
+            if pt.code:
+                pt.display_name = "[%s] %s"% (pt.code, pt.display_name)
